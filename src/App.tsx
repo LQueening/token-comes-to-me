6f598c32ec839a003cf9ad3a068a969a778e43e4
廖锟 <liaokun@liaokundePro.lan>
2023-09-15 22:14:06 +0800

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Header from "../";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="App">123456</div>;
+}
+
+export default App;
