9bc8fcaa1af2f92d40ce499dd78ec19a1d841d12
廖锟 <liaokun@liaokundePro.lan>
2023-09-16 16:38:55 +0800

    feat: Add connect wallet process

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
 import { useState } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { WagmiConfig, createConfig, configureChains } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import * as chain from "wagmi/chains";
+import Header from "./components/Header";
+import Content from "./components/Content";
 import "./App.css";
+
+const { chains, publicClient } = configureChains(
+  [chain.mainnet, chain.bsc, chain.arbitrum, chain.polygon, chain.optimism],
+  [publicProvider()]
+);
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  connectors: [
+    new InjectedConnector({
+      chains,
+      options: {
+        name: "Injected",
+        shimDisconnect: true,
+      },
+    }),
+  ],
+});
 
 function App() {
   const [count, setCount] = useState(0);
-
-  return <div className="App">123456</div>;
+  return (
+    <WagmiConfig config={config}>
+      <ChakraProvider>
+        <div className="App">
+          {/* <Header></Header> */}
+          <Content></Content>
+        </div>
+      </ChakraProvider>
+    </WagmiConfig>
+  );
 }
 
 export default App;
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,10 +1,64 @@
-import { useState } from "react";
-import "./App.css";
+import { useConnect, useAccount } from "wagmi";
+import { Container, Image, Box } from "@chakra-ui/react";
+import taroBg from "../assets/taro-bg.png";
+import taroLogo from "../assets/taro-logo.png";
 
 const Content = () => {
-  const [count, setCount] = useState(0);
+  const { address } = useAccount();
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect();
 
-  return <div className="App">123456</div>;
+  console.log(address);
+
+  return (
+    // Container box, limit the Dapp size
+    <Container
+      position="absolute"
+      padding="0"
+      left={{ base: "0", md: "50%" }}
+      top={{ base: "0", md: "10%" }}
+      transform={{
+        base: "",
+        md: "translateX(-50%)",
+      }}
+      w={{ base: "100%", md: "375px" }}
+      h={{ base: "100%", md: "667px" }}
+      boxShadow="0px 6px 20px rgba(0, 0, 0, 0.25)"
+    >
+      {/* Show connect wallet page when there's no address reading */}
+      {!address && (
+        <Box
+          position="relative"
+          h="100%"
+          backgroundImage={taroBg}
+          backgroundSize="100%"
+          backgroundRepeat="no-repeat"
+          backgroundPosition="center"
+        >
+          <Box
+            w="60%"
+            position="absolute"
+            left="50.5%"
+            top="47.2%"
+            transform="translate(-50%, -50%)"
+          >
+            {connectors.map((connector, connectorIndex) => (
+              <Image
+                key={connectorIndex}
+                animation="rotate 60s linear infinite"
+                cursor="pointer"
+                _hover={{
+                  transform: "scale(1.05)",
+                }}
+                src={taroLogo}
+                onClick={() => connect({ connector })}
+              ></Image>
+            ))}
+          </Box>
+        </Box>
+      )}
+    </Container>
+  );
 };
 
 export default Content;
